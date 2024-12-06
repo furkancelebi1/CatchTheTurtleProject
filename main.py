5b782b81f2fe57efe7c7a8933f5307db9cf56aa0
Furkan Çelebi <ffurkancelebi@gmail.com>
2024-12-06 22:10:45 +0300

    test modev to top

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ screen = turtle.Screen()
 screen.bgcolor("yellow")
 screen.title("Catch The Turtle")
 font = ("Arial",15,"bold")
-#score turtle
-
 score_turtle = turtle.Turtle()
-score_turtle.color("dark blue")
-score_turtle.write(arg="Score: 0", move=False, align="center", font=font)
+#score turtle
+def setup_score_turtle():
+    score_turtle.color("dark blue")
+    score_turtle.ht()
+    score_turtle.penup()
 
-
-
-
-
-
-
-
+    top_height = screen.window_height() / 2
+    y = top_height * 0.9
+    score_turtle.setposition(0,y)
+    score_turtle.write(arg="Score: 0", move=False, align="left", font=font)
+setup_score_turtle()
 turtle.mainloop()
